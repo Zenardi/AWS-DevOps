160d1ef212e916a8da4a2f879ffc2a44405dfada
zenardi <du.zenardi@gmail.com>
2019-11-04 15:27:56 -0800

    kubernetes deployment

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=zenardi/udacity-pred-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-pred-app --image=zenardi/udacity-pred-app --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment udacity-pred-app --type=LoadBalancer --port=8000 --target-port=80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=zenardi/udacity-pred-app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag v1 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
